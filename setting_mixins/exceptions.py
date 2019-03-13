490b16209d3353f749291cebc50ce5e6c275d964
= <jacepetkau@hotmail.com>
2019-03-13 15:26:54 +0000

    Adding custom exceptions for the setting mixin factory.

---
+++ setting_mixins/exceptions.py
@@ -0,0 +1,21 @@
+__all__ = ('MissingSettingNames', 'MultipleSettingDefaults',)
+
+
+class MissingSettingNames(Exception):
+    """The factory wasn't passed the names of any settings.
+
+    The factory function expects to receive the name of at least one
+    setting.
+    """
+    pass
+
+
+class MultipleSettingDefaults(Exception):
+    """The factory was passed multiple names with kwargs.
+
+    When the factory function is passed the names of multiple settings
+    there shouldn't be a default value or a setting label also passed
+    to the function. If we allowed this the same label and value would
+    be set for each name.
+    """
+    pass
